a2e3e789cc9bc6bdb26b3123339ece91dbecb3e2
nafiz-al-din <ndin@cisscom.com>
2022-05-16 14:04:17 +0600

    first push

---
+++ demo/background.js
@@ -0,0 +1,30 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('hello world');
+    console.log({"extentionID": chrome.runtime.id});
+    // window.open("https://www.twitter.com/", '_blank');
+    console.log({"chrome class": chrome});
+
+    const url = chrome.extension.getURL('http://localhost:3000')
+    
+    // chrome.permissions.getAll(perms => {
+    //     console.log(perms);
+    // });
+
+    chrome.storage.sync.set({name: 'nafiz', data: ['asd', 123]})
+
+    chrome.storage.sync.get('name', (data) => {
+        console.log(data);
+    });
+
+    chrome.i18n.detectLanguage('en', (data) => {
+        console.log(data);
+    });
+
+    // console.log(chrome.instanceID);
+    
+});
+
+chrome.bookmarks.onCreated.addListener(function() {
+    console.log({"bookmarks chrome class": chrome.bookmarks});
+    console.log('changed');
+});
